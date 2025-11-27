f8ad167d7dac567408f92496707804b8b5eea7fb
Shubhamp11 <56792673+Shubhamp11@users.noreply.github.com>
2025-11-27 20:08:40 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "MyAI3",
-  description: "MyAI3",
+  title: "CompliBot",
+  description: "CompliBot",
 };
 
 export default function RootLayout({
